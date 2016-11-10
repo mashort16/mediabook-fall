39ccd4c1972b218eed2c15c9e73137af6c079bbc
Megan Short <meganalysse.short@gmail.com>
2016-11-10 08:40:41 -0500

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+
+
+  root 'welcome#index'
+  devise_for :users
+
+  resources :friendships, only: [:create, :destroy, :accept] do
+    member do
+      put :accept
+    end
+  end
+
+  resources :activities, only: [:index]
+
+  resources :posts, only: [:edit, :create, :update, :destroy]
+
+  resources :users, only: [:show, :index]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
